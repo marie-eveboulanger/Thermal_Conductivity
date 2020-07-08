23f9a294eb7ab3a325652b878d5a543aba92ac93
Alexandre Dumont <Alexandre.Dumont3@usherbrooke.ca>
2020-07-08 16:41:49 -0400

    Added array-like functionality to Measurement in Graph.py

--- Graphs.py
+++ Graphs.py
@@ -36,91 +36,111 @@ class Measurment():
     """
 
     # Creation of a dictionnary to sort data
-    measures = dict()
-    measures["T_av"] = ["T_av(K)", "Taverage(K)", "T (K)"]
-    measures["T0"] = ["T0(K)", "T0 (K)"]
-    measures["Tp"] = ["T+(K)", "T+ (K)"]
-    measures["Tm"] = ["T-(K)", "T- (K)"]
-    measures["dTx"] = ["dTx(K)", "dTx (K)"]
-    measures["kxx"] = ["kxx(W/Km)", "k_xx(W/Km)", "Kxx (W / K m)"]
-    measures["kxy"] = ["kxy(W/mk)", "k_xy(W/Km)", "Kxy (W / K m)"]
-    measures["dTy"] = ["dTy(K)", "dTy (K)"]
-    measures["I"] = ["I(A)", "I (A)"]
-    measures["dTabs"] = ["dTabs", "dT_abs"]
+    __dict_measures = dict()
+    __dict_measures["T_av"] = ["T_av(K)", "Taverage(K)", "T (K)"]
+    __dict_measures["T0"] = ["T0(K)", "T0 (K)"]
+    __dict_measures["Tp"] = ["T+(K)", "T+ (K)"]
+    __dict_measures["Tm"] = ["T-(K)", "T- (K)"]
+    __dict_measures["dTx"] = ["dTx(K)", "dTx (K)"]
+    __dict_measures["kxx"] = ["kxx(W/Km)", "k_xx(W/Km)", "Kxx (W / K m)"]
+    __dict_measures["kxy"] = ["kxy(W/mk)", "k_xy(W/Km)", "Kxy (W / K m)"]
+    __dict_measures["dTy"] = ["dTy(K)", "dTy (K)"]
+    __dict_measures["I"] = ["I(A)", "I (A)"]
+    __dict_measures["dTabs"] = ["dTabs", "dT_abs"]
 
     # Creation of a dictionnary to sort other info
-    parameters = dict()
-    parameters["H"] = ["H"]
-    parameters["w"] = ["w"]
-    parameters["t"] = ["t"]
-    parameters["L"] = ["L"]
-    parameters["sample"] = ["Sample", "sample"]
+    __dict_parameters = dict()
+    __dict_parameters["H"] = ["H"]
+    __dict_parameters["w"] = ["w"]
+    __dict_parameters["t"] = ["t"]
+    __dict_parameters["L"] = ["L"]
+    __dict_parameters["sample"] = ["Sample", "sample"]
 
-    def __init__(self, filename, H=None, w=None, t=None, L=None, sample=None):
+    def __init__(self, filename=None, H=None, w=None, t=None, L=None, sample=None):
         """
         Used to initialize a Measurement object
 
         Parameters:
-        ------------------------------------------------------------------------
-        filename :  str
-                The path to the file containing the data to be read and stored.
-                Can be relative or absolute.
-        H, w, t, L :    int or float
-                The value of the magnetic field used during the experiment and
-                the geometric parameters of the sample.
-        sample_name :   str
-                The name of the sample.
-        """
+            ------------------------------------------------------------------------
+            filename :  str
+            The path to the file containing the data to be read and stored.
+            Can be relative or absolute.
+            H, w, t, L :    int or float
+            The value of the magnetic field used during the experiment and
+            the geometric parameters of the sample.
+            sample_name :   str
+            The name of the sample.
+            """
 
+        self.measures = []
+        self.parameters = []
         # Sets the values using the info provided
-        # if H is not None:
-        #    self.H = H
+        if H is not None:
+            self.H = H
+
         if w is not None:
             self.w = w
+
         if t is not None:
             self.t = t
+
         if L is not None:
             self.L = L
+
         if sample is not None:
             self.sample = sample
 
-        filename = os.path.abspath(filename)
-        raw_data = np.genfromtxt(filename, delimiter="\t", skip_header=1).T
-        lines = []
-        with open(filename) as f:
-            for i, line in enumerate(f):
-                l = line.split(" ")
-                if l[0] != "#":
-                    numbers = ["0", "1", "2", "3",
-                               "4", "5", "6", "7", "8", "9"]
-                    if l[0][0] in numbers:
-                        break
+        if filename is not None:
+            filename = os.path.abspath(filename)
+            raw_data = np.genfromtxt(filename, delimiter="\t", skip_header=1).T
+            lines = []
+            with open(filename) as f:
+                for i, line in enumerate(f):
+                    l = line.split(" ")
+                    if l[0] != "#":
+                        numbers = ["0", "1", "2", "3",
+                                   "4", "5", "6", "7", "8", "9"]
+                        if l[0][0] in numbers:
+                            break
+                        else:
+                            lines.append(l[0].strip())
                     else:
-                        lines.append(l[0].strip())
+                        lines.append(line.strip()[2:])
+
+            #Should contain all the comment lines without trailing \n and
+            #starting #
+            self.lines = lines
+
+            # Separate misc comments from the actual header
+            for line in self.lines:
+                l = line.split("\t")
+                if len(l) < 6:
+                    for key, values in self.__dict_parameters.items():
+                        if l[0] in values:
+                            if hasattr(self, key) is False:
+                                setattr(self, key, l[-1])
+                                self.parameters.append(key)
+                            else:
+                                pass
+                        else:
+                            pass
                 else:
-                    lines.append(line.strip()[2:])
+                    for key, values in self.__dict_measures.items():
+                        for i in range(len(l)):
+                            if l[i].strip() in values:
+                                setattr(self, key, raw_data[i])
+                                self.measures.append(key)
+                            else:
+                                pass
 
-        #Should contain all the comment lines without trailing \n and starting #
-        self.lines = lines
-
-        # Separate misc comments from the actual header
-        for line in self.lines:
-            l = line.split("\t")
-            if len(l) < 6:
-                for key, values in self.parameters.items():
-                    if l[0] in values:
-                        if hasattr(self, key) is False:
-                            setattr(self, key, l[-1])
+            if hasattr(self, "sample") is False:
+                self.sample = "unknown"
             else:
-                for key, values in self.measures.items():
-                    for i in range(len(l)):
-                        if l[i].strip() in values:
-                            setattr(self, key, raw_data[i])
-
-        if hasattr(self, "sample") is False:
-            self.sample = "unknown"
-        if hasattr(self, "H") is False:
-            self.H = "unknown"
+                pass
+            if hasattr(self, "H") is False:
+                self.H = "unknown"
+            else:
+                pass
 
         return
 
@@ -137,17 +157,41 @@ class Measurment():
             else:
                 string = "Measurement of %s sample at %s H" % (
                     self.sample, self.H)
+
         return string
 
     def __call__(self):
-        if hasattr(self, "kxy") is False:
-            data = np.array([self.T_av, self.T0, self.Tp,
-                             self.Tm, self.dTx, self.kxx])
-        else:
-            data = np.array([self.T_av, self.T0, self.Tp,
-                             self.Tm, self.dTx, self.kxx,self.dTy,self.kxy])
-
+        """
+        Returns the transposed raw data
+        """
+        data = np.array([getattr(self,key) for key in self.measures])
         return data
+
+    def __getitem__(self, key):
+        if type(key) is str:
+            return getattr(self,key)
+        else:
+            M = Measurment()
+            for i in self.measures:
+                setattr(M,i,getattr(self,i)[key])
+
+            for i in self.parameters:
+                setattr(M,i,getattr(self,i))
+
+            M["measures"] = self.measures
+            M["parameters"] = self.parameters
+            return M
+
+    def __setitem__(self, key, value):
+        if type(key) is str:
+            setattr(self,key,value)
+        else:
+            pass
+        return
+
+    def __delitem__(self, key):
+        delattr(self, key)
+        return
 
 class Data_Set():
     """
