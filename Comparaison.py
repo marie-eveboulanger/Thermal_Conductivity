09f8e24eb0e40e95b8ca96a734180ff48b19fd19
Alexandre <alexandre.dumont3@usherbrooke.ca>
2020-07-15 10:03:47 -0400

    Added Plot_fancy to Data_Set in Comparaison.py

--- Comparaison.py
+++ Comparaison.py
@@ -436,6 +436,34 @@ class Data_Set():
 
         return
 
+    def __create_grid(self, measures):
+        n = len(measures)
+
+        if n % 2 == 0:
+            N = n//2
+            fig, axes = plt.subplots(N, 2, figsize=(16, N*4.5))
+        else:
+            N = n//2+1
+            s = (N, 4)
+            fig, ax = plt.subplots(N, 2, figsize=(16, N*4.5))
+            axes = []
+            loc = (0, 0)
+            for i in range(n):
+
+                if i != 0:
+                    if i != n-1:
+                        if int(loc[1]) == 0:
+                            loc = (loc[0], 2)
+                        elif int(loc[1]) == 2:
+                            loc = (loc[0]+1, 0)
+                    else:
+                        loc = (N-1, 1)
+                else:
+                    pass
+                axes.append(plt.subplot2grid(s, loc, colspan=2, fig=fig))
+
+        return fig, axes
+
     def Plot(self, key, *args, **kwargs):
         """
         Plots data corresponding to key for all Measurement in Data_Set
@@ -541,7 +569,7 @@ class Data_Set():
             kwargs.pop("ax")
             return_fig = False
         except KeyError:
-            fig, ax = plt.subplots(figsize=(8,4.5))
+            fig, ax = plt.subplots(figsize=(8, 4.5))
             return_fig = True
 
         zero_line = 0
@@ -649,3 +677,63 @@ class Data_Set():
             pass
 
         return
+
+    def Plot_fancy(self, *args, **kwargs):
+        """
+        Just like Plot_all but with a fancy layout that is more suited to
+        ipython notebooks
+        """
+
+        remove = ["T_av", "T0", "Tp", "Tm"]
+        if len(self.measurements) > 1:
+            remove.append("Tp_Tm")
+        else:
+            pass
+        measures = [i for i in self.measures if i not in remove]
+        ref_meas = ["kxx", "kxx/T", "kxy", "kxy/T", "dTx",
+                    "dTx/T", "dTy/dTx", "Resistance"]
+        measures = [i for i in ref_meas if i in measures]
+
+        fig, ax = self.__create_grid(measures)
+
+        try:
+            kwargs.pop("show")
+        except KeyError:
+            pass
+
+        # Tries to find sample name
+        if "sample" in self.parameters:
+            sample = self.measurements[0]["sample"]
+            for m in self.measurements:
+                if sample == m["sample"]:
+                    pass
+                else:
+                    sample = None
+                    break
+
+        try:
+            filename = kwargs["filename"]
+            kwargs.pop("filename")
+        except KeyError:
+            filename = None
+
+        for i in range(len(measures)):
+            self.Plot(measures[i], *args, show=None,
+                      fig=fig, ax=ax[i], **kwargs)
+
+        if sample is not None:
+            plt.suptitle(sample,fontsize=22)
+        else:
+            pass
+
+        fig.tight_layout(rect=[0, 0.03, 1, 0.95])
+
+        if filename is not None:
+            filename = os.path.abspath(filename)
+            pp = PdfPages(filename)
+            pp.savefig(fig)
+            pp.close()
+        else:
+            pass
+
+        return fig, ax
