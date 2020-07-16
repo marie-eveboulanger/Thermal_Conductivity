6db20f2c2e48d2df30ea3f300fba23192016a654
Alexandre <alexandre.dumont3@usherbrooke.ca>
2020-07-16 16:33:30 -0400

    Modified setup.py for v2.1 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ThermalConductivity", # Replace with your own username
-    version="2.0",
+    version="2.1",
     author="Alexandre Dumont",
     author_email="alexandre.dumont3@usherbrooke.ca",
     description="A module to analyze thermal conductivity measurements",
