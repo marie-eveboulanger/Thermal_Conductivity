38c4ff3e8df4020f6440051e9345ae659d846394
marie-eveboulanger <marieeve.boulanger.1@gmail.com>
2020-07-17 14:11:52 -0400

    format file

--- tests/test_that_nothing_is_broken.py
+++ tests/test_that_nothing_is_broken.py
@@ -5,9 +5,8 @@ However, this file can be run to have an idea that everything seems fine.
 
 from ThermalConductivity import Analysis
 
-filename = "Data-TS-0.0T-BOT-SCOC_1903A-2019-04-09.dat"
+FILENAME = "Data-TS-0.0T-BOT-SCOC_1903A-2019-04-09.dat"
 
-conductivity = Analysis.Conductivity(filename)
+conductivity = Analysis.Conductivity(FILENAME)
 
 conductivity.Plot("kxx", "-o")
-
