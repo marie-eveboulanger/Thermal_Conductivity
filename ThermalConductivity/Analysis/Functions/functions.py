8b48d818d0964feeb75ff12203b1ddcd01a773fa
Alexandre <alexandre.dumont3@usherbrooke.ca>
2020-07-17 16:06:50 -0400

    Added functions to Analysis.Functions

--- ThermalConductivity/Analysis/Functions/functions.py
+++ ThermalConductivity/Analysis/Functions/functions.py
@@ -1,6 +1,9 @@
 """
 This subsubmodule contains detailled versions of useful functions
 """
+import numpy as np
+import numpy.polynomial as npp
+from ThermalConductivity.Thermometry import seebeck_thermometry
 
 
 def compute_kxx(I, dTx, width, thickness, length, Resistance=5000):
@@ -53,3 +56,90 @@ def compute_kxy(kxx, dTx, dTy, width, length, Resistance=5000):
     kxy = kxx*delta_ratio*geo_factor
 
     return kxy
+
+
+def tallahassee_temp(Resistance_heat_off, Resistance_heat_on, T0, order=8):
+    """
+    Computes either T+ or T- since it's the same calculations
+
+    Parameters:
+    ----------------------------------------------------------------------------
+    Resistance_heat_off:    1d array
+                        The values of the resistance without heat in volts
+    Resistance_heat_on:     1d array
+                        The values of the resistance with heat in volts
+    T0:                     1d array
+                        The values of the probe's base temperature in Kelvin
+    order:                  int
+                        The order of the polynomial fit
+    """
+
+    # Polynomial fit on the heat off values to "calibrate"
+    # Log is used to increase the precision of the fit
+    Coefficients = npp.polyfit(np.log(Resistance_heat_off), np.log(T0), order)
+
+    # Use the fit to get the calibrated heat on temperature
+    Temperature = np.exp(npp.polyval(np.log(Resistance_heat_on), Coefficients))
+
+    return Temperature
+
+
+def compute_thermocouple(dT_heat_off, dT_heat_on, T_reference, gain=1000):
+    """
+    Computes the calibrated difference in temperature from a thermocouple
+
+    Parameters:
+    ----------------------------------------------------------------------------
+    dT_heat_off:    1d array
+                    The difference in temperatures when the heater is off
+    dT_heat_on:     1d array
+                    The difference in temperatures when the heater is on
+    T_ref:          The temperature of the point at which the thermocouple
+                    is connected
+    gain:           float or int
+                    The preamp gain
+    """
+
+    seebeck_coefficients = seebeck_thermometry(T_reference)
+    delta_T = abs(dT_heat_on-dT_heat_off)/seebeck_coefficients/gain
+
+    return delta_T
+
+
+def VTI_Calibration_Loop(dT_abs_off, dT_abs_on, dTx_off, dTx_on, T0):
+    """
+    Computes a calibration loop for the VTI's thermocouple
+
+    Parameters:
+    ----------------------------------------------------------------------------
+    dT_abs_off:     1d array
+                    The readings of the absolute thermocouple with no heat
+    dT_abs_on:      1d array
+                    The readings of the absolute thermocouple with heat
+    dTx_off:        1d array
+                    The reading of the thermocouple along x with no heat
+    dTx_on:         1d array
+                    The reading of the thermocouple along x with heat
+    T0:             1d array
+                    The temperature of the probe
+    """
+
+    # First iteration
+    dT_abs = compute_thermocouple(dT_abs_off, dT_abs_on, T0)
+    dTx = compute_thermocouple(dTx_off, dTx_on, T0)
+    T_minus = T0+dT_abs
+    T_plus = T_minus+dTx
+    T_av = 0.5*(T_minus+T_plus)
+    previous_T_av = T_av+1000
+
+    while abs(previous_T_av-T_av) > 1e-10:
+        previous = T_av
+        T_ref_1 = T0+dT_abs/2
+        T_ref_2 = T_minus+dTx/2
+        dT_abs = compute_thermocouple(dT_abs_off, dT_abs_on, T_ref_1)
+        dTx = compute_thermocouple(dTx_off, dTx_on, T_ref_2)
+        T_minus = T0+dT_abs
+        T_plus = T_minus+dTx
+        T_av = 0.5*(T_minus+T_plus)
+
+    return T_av, dTx, T_plus, T_minus
