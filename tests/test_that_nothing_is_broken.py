fa33a3d63c11cc2990b0222c8896587379b0a3c4
Alexandre <alexandre.dumont3@usherbrooke.ca>
2020-07-23 15:15:08 -0400

    Modified test script slightly

--- tests/test_that_nothing_is_broken.py
+++ tests/test_that_nothing_is_broken.py
@@ -4,8 +4,11 @@ However, this file can be run to have an idea that everything seems fine.
 """
 
 from ThermalConductivity import Analysis
+import os
 
+DIR = os.path.dirname(os.path.realpath(__file__))
 FILENAME = "Data-TS-0.0T-BOT-SCOC_1903A-2019-04-09.dat"
+FILENAME = os.path.join(DIR,FILENAME)
 
 conductivity = Analysis.Conductivity(FILENAME)
 
